649213de7aa7cdf4110782e401136a0bf3e50683
yaw owusu <asirifiyaw@gmail.com>
2017-11-05 20:12:23 -0800

    added exphbs code and specified routes

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("views/public/js"));
+app.use(express.static("views/public/css"));
+app.use(express.static("views/public/img"));
+
+app.use(bodyParser.urlencode({
+	extended:false
+}));
+app.use(methodOverride("_method"));
+
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+
+
 app.listen(PORT, function(){
 	console.log("App listening on port: "+ PORT);
 })
+
+
+module.exports = app;
