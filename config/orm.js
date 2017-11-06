04857135f291b6fafe078b4e63ff0661c11701fd
yaw owusu <asirifiyaw@gmail.com>
2017-11-06 13:25:22 -0800

    refactored orms

--- config/orm.js
+++ config/orm.js
@@ -8,13 +8,13 @@ var orm = {
             cb(result);
         });
     },
-    eatBurger: function(tableInput, burger_name, cb) {
-        var s = 'UPDATE ' + tableInput + "SET devoured=true WHERE burger_name = '" + burger_name + "';";
-        connection.query(s, function(err, result) {
-            if (err) throw err;
-            cb(result);
-        });
-    },
+    // eatBurger: function(tableInput, burger_name, cb) {
+    //     var s = 'UPDATE ' + tableInput + "SET devoured=true WHERE burger_name = '" + burger_name + "';";
+    //     connection.query(s, function(err, result) {
+    //         if (err) throw err;
+    //         cb(result);
+    //     });
+    // },
     addBurger: function(tableInput, burger_name, cb) {
         var s = "INSERT INTO " + tableInput + "(burger_name, devoured) VALUES ('" + burger_name + "', false );";
         connection.query(s, function(err, result) {
@@ -37,7 +37,7 @@ var orm = {
         });
     },
     addBurger: function(tableInput, burger_name, cb) {
-        var s = "INSERT INTO " + tableInput + " (burger_name, devoured) VALUES ('" +burger_name+ "',false);";
+        var s = "INSERT INTO " + tableInput + " (burger_name, devoured) VALUES ('" + burger_name + "',false);";
         connection.query(s, function(err, result) {
             if (err) throw err;
             cb(result);
