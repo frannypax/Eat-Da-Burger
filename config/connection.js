d28bd6cc75f83e604b3b61939e3a2f1910dfcb58
yaw owusu <asirifiyaw@gmail.com>
2017-11-05 22:35:14 -0800

    creating db connection

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+//connecting to mysql db
+
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	user:"root",
+	password:"root",
+	database: "burgers_db"
+});
+connection.connect(function(err){
+	if(err){
+		throw err
+	}
+	console.log("Connected as id " + connection.threadId);
+});
+
+module.exports = connection;
