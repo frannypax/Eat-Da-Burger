3bf8050fe1a869273103591dc9a436dc9ddbfc55
yaw owusu <asirifiyaw@gmail.com>
2017-11-07 13:05:45 -0800

    refactoring orms

--- config/orm.js
+++ config/orm.js
@@ -1,41 +1,36 @@
-var connection = require('../config/connection.js');
+
+//importing connections 
+var connection = require('./connection.js');
 
 var orm = {
     selectAll: function(tableInput, cb) {
-        var s = 'SELECT * FROM ' + tableInput + ';';
-        connection.query(s, function(err, result) {
+        var queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             cb(result);
         });
     },
     addBurger: function(tableInput, burger_name, cb) {
-        var s = "INSERT INTO " + tableInput + "(burger_name, devoured) VALUES ('" + burger_name + "', false );";
-        connection.query(s, function(err, result) {
+        var queryString = "INSERT INTO " + tableInput + "(burger_name, devoured) VALUES ('" + burger_name + "', false );";
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             cb(result);
         });
     },
     deleteBurger: function(tableInput, burger_name, cb) {
-        var s = "DELETE FROM " + tableInput + " WHERE burger_name = '" + burger_name + "';";
-        connection.query(s, function(err, result) {
+        var queryString = "DELETE FROM " + tableInput + " WHERE burger_name = '" + burger_name + "';";
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             cb(result);
         });
     },
     eatBurger: function(tableInput, burger_name, cb) {
-        var s = "UPDATE " + tableInput + " SET devoured = 1 WHERE burger_name = '" + burger_name + "';";
-        connection.query(s, function(err, result) {
+        var queryString = "UPDATE " + tableInput + " SET devoured = 1 WHERE burger_name = '" + burger_name + "';";
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             cb(result);
         });
     },
-    addBurger: function(tableInput, burger_name, cb) {
-        var s = "INSERT INTO " + tableInput + " (burger_name, devoured) VALUES ('" + burger_name + "',false);";
-        connection.query(s, function(err, result) {
-            if (err) throw err;
-            cb(result);
-        });
-    }
 };
 
 module.exports = orm;
