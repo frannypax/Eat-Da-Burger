84a0196a5a77d73687bbf2f74dde99f506180526
yaw owusu <asirifiyaw@gmail.com>
2017-11-09 08:01:18 -0800

    configured jawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,27 @@
 
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+// 
+// for heroku connection
+var connection;
+
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+	var connection = mysql.createConnection({
 	host: "localhost",
 	user:"root",
 	password:"root",
 	database: "burgers_db"
-});
+	});
+};
 connection.connect(function(err){
 	if(err){
 		console.log("Error" + err);
 	}
 	console.log("Connected as id " + connection.threadId);
 });
+
 
 module.exports = connection;
